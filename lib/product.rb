d1d53809b9c999cb028e2e146de2b3fba5eaffae
Chris Saden <chris.saden@udacity.com>
2016-05-01 11:23:12 -0700

    implement DuplicateProductError and addtoproducts

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,26 @@
+require_relative "./errors"
+
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    if @@products.include?(self)
+      @@products << self
+    else
+      raise DuplicateProductError.new "#{@title} already exists."
+    end
   end
 end
