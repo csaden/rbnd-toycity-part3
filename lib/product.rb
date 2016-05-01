0bf58b6a97be6d6a1b3f7e830b1b5aa02ff84203
Chris Saden <chris.saden@udacity.com>
2016-05-01 12:54:21 -0700

    implement product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,31 @@
 require_relative "./errors"
 
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
+  end
+
+  def decrease_stock(amount=1)
+    @stock -= amount
+  end
+
+  def in_stock?
+    @stock > 0
+  end
+
+  def self.in_stock
+    @@products.find_all {|product| product.in_stock?}
+  end
+
+  def self.find_by_title(title)
+    @@products.find {|product| product.title == title}
   end
 
   def self.all
@@ -17,10 +35,10 @@ class Product
   private
 
   def add_to_products
-    if @@products.include?(self)
-      @@products << self
-    else
+    if @@products.map{|product|product.title}.include?@title
       raise DuplicateProductError.new "#{@title} already exists."
+    else
+      @@products << self
     end
   end
 end
